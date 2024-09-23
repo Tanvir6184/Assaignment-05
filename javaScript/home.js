bc995f3441a1ad4fbe784fb6030c3df4a45b66d1
Tanvir6184 <goldenittanvirzain61@gmail.com>
2024-09-23 21:19:03 +0600

    Modal and others section added

--- javaScript/home.js
+++ javaScript/home.js
@@ -1,10 +1,40 @@
 
-document.getElementById('btn-history').addEventListener('click', function(){
-    window.location.href = 'history.html'
-})
+// document.getElementById('btn-history').addEventListener('click', function(){
+//     window.location.href = 'history.html'
+// })
 
 
 
-document.getElementById('nav-blog-button').addEventListener('click', function(){
-    window.location.href = 'blog.html'
+// document.getElementById('nav-blog-button').addEventListener('click', function(){
+//     window.location.href = 'blog.html'
+    
+// })
+
+
+
+
+const addedAmount = document.getElementById('donation-added-amount')
+
+
+
+document.getElementById('card-one-main-btn').addEventListener('click', function(){
+
+    const inputFieldMoney = getInputFieldValueId('card-one-input-field') ;
+
+
+    if(isNaN(inputFieldMoney) || inputFieldMoney === "" ){
+        alert('Please write valid number');
+    }
+    else{
+    
+    
+        const fieldToNumber = getAddedAmountValueById('donation-added-amount');
+        const TotalAmount = fieldToNumber + inputFieldMoney;
+        document.getElementById('donation-added-amount').innerText = TotalAmount;
+        
+        const minusFunction = getMinusFigure('minus-Amount');
+        const totalMinusAmount = minusFunction - inputFieldMoney;
+        document.getElementById('minus-Amount').innerText = totalMinusAmount;
+        
+    }
 })
--- javaScript/utilities.js
+++ javaScript/utilities.js
@@ -1,5 +1,18 @@
 
-document.getElementById('').addEventListener('click', function(){
+function getInputFieldValueId(id){
+ const moneyAddedBtn = document.getElementById(id).value;
+ const inputNumber = parseFloat(moneyAddedBtn);
+ return inputNumber;
+}
 
-    
-})
+function getAddedAmountValueById(id){
+    const addedAmountField = document.getElementById(id).innerText;
+    const addedAmountFieldNumber = parseFloat(addedAmountField);
+    return addedAmountFieldNumber;
+}
+
+function getMinusFigure(id){
+    const minusAmountText = document.getElementById(id).innerText;
+    const minusAmountNumber = parseFloat(minusAmountText);
+    return minusAmountNumber;
+}
