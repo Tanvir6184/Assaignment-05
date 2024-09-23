e33ae5eeab05a61b2cc13f54fee8c3956ed6110e
Tanvir6184 <goldenittanvirzain61@gmail.com>
2024-09-23 17:34:24 +0600

    blog page commited

--- javaScript/home.js
+++ javaScript/home.js
@@ -2,3 +2,9 @@
 document.getElementById('btn-history').addEventListener('click', function(){
     window.location.href = 'history.html'
 })
+
+
+
+document.getElementById('nav-blog-button').addEventListener('click', function(){
+    window.location.href = 'blog.html'
+})
---
+++ javaScript/utilities.js
@@ -0,0 +1,5 @@
+
+document.getElementById('').addEventListener('click', function(){
+
+    
+})
